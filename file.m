07fa52692d6b15931cb8c7fc469fe536154d7f43
Great-Dango <74141314+Great-Dango@users.noreply.github.com>
2021-03-22 15:56:16 +0300

    Update file.m

--- file.m
+++ file.m
@@ -1,13 +1,20 @@
-[F, err] = fopen('output.txt','w');
-if (F == -1)
-    fprintf('%s\n',err);
-else
-    A = rand(3);
-    for i = 1:3
-        for j = 1:3
-            fprintf(F, '%.4f ', A(i,j));
+%writing a 3x3 matrix to a file
+function write_matrix(filename)
+    if exist(filename)
+        [F, err] = fopen(filename,'w');
+        if F == -1
+            fprintf('%s\n',err);
+        else
+            A = rand(3);
+            for i = 1:3
+                for j = 1:3
+                    fprintf(F, '%.4f ', A(i,j));
+                end
+                fprintf(F, '\n');
+            end
+            fclose(F);
         end
-        fprintf(F, '\n');
+    else
+        fprintf('File no exists\n');
     end
 end
-fclose(F);
